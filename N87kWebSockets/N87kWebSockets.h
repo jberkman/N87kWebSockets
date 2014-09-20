95519ba7184bcad8b3e19e273434c2ad35cfafc5
jacob berkman <jacob@87k.net>
2014-09-20 13:40:51 -0700

    Initial Commit

---
+++ N87kWebSockets/N87kWebSockets.h
@@ -0,0 +1,19 @@
+//
+//  N87kWebSockets.h
+//  N87kWebSockets
+//
+//  Created by jacob berkman on 9/20/14.
+//  Copyright (c) 2014 87k Networks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for N87kWebSockets.
+FOUNDATION_EXPORT double N87kWebSocketsVersionNumber;
+
+//! Project version string for N87kWebSockets.
+FOUNDATION_EXPORT const unsigned char N87kWebSocketsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <N87kWebSockets/PublicHeader.h>
+
+
