3e415e0c7718352fe016e844d613ad421bb53a66
jacob berkman <jacob@87k.net>
2014-09-21 01:39:14 -0700

    initial send & receive (but not process) handshake

--- N87kWebSockets/WebSocket.swift
+++ N87kWebSockets/WebSocket.swift
@@ -25,6 +25,7 @@
 //
 
 import Foundation
+import Security
 
 public enum Scheme: String {
     case WS = "ws"
@@ -39,7 +40,16 @@ public class WebSocket: NSObject {
     private enum State {
         case Connecting, Open, Closing, Closed
     }
-    private var state = State.Connecting
+    private var state: State = .Connecting {
+        didSet {
+            switch state {
+            case .Connecting:
+                key = nil
+            default:
+                break
+            }
+        }
+    }
 
     private var _currentRequest: NSURLRequest
 
@@ -47,6 +57,9 @@ public class WebSocket: NSObject {
 
     public let originalRequest: NSURLRequest
     public var currentRequest: NSURLRequest { return _currentRequest }
+    private var scheme: Scheme? {
+        return currentRequest.URL.scheme != nil ? Scheme.fromRaw(currentRequest.URL.scheme!) : nil
+    }
 
     public let subprotocols: [String]
 
@@ -82,24 +95,115 @@ extension WebSocket {
 }
 
 extension WebSocket {
+
     private func connect() {
         if state != .Connecting {
             return
         }
 
-        let URL = currentRequest.URL
-        let scheme = URL.scheme != nil ? Scheme.fromRaw(URL.scheme!) : nil
-
-        if URL.host == nil || scheme == nil {
+        if currentRequest.URL.host == nil || scheme == nil || currentRequest.HTTPMethod != "GET" {
             state = .Closed
             let error = NSError(domain: NSCocoaErrorDomain, code: NSURLErrorBadURL, userInfo: nil)
             NSTimer.scheduledTimerWithTimeInterval(0, target: self, selector: "delegateErrorTimerDidFire", userInfo: error, repeats: false)
             return
         }
 
-        NSLog("Getting streams to %@...", "\(URL.host)")
-        NSStream.getStreamsToHostWithName(URL.host!, port: URL.port?.integerValue ?? scheme!.defaultPort, inputStream: &input, outputStream: &output)
-        NSLog("Done: %@ %@", "\(input)", "\(output)")
+        let port = currentRequest.URL.port?.integerValue ?? scheme!.defaultPort
+        NSStream.getStreamsToHostWithName(currentRequest.URL.host!, port: port, inputStream: &input, outputStream: &output)
+
+        input?.delegate = self
+        output?.delegate = self
+
+        let securityLevel = scheme!.isSecure ? NSStreamSocketSecurityLevelTLSv1 : NSStreamSocketSecurityLevelNone
+        input?.setProperty(securityLevel, forKey: NSStreamSocketSecurityLevelKey)
+        output?.setProperty(securityLevel, forKey: NSStreamSocketSecurityLevelKey)
+
+        input?.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+        output?.scheduleInRunLoop(NSRunLoop.currentRunLoop(), forMode: NSDefaultRunLoopMode)
+
+        input?.open()
+        output?.open()
+    }
+
+}
+
+extension WebSocket {
+
+    private func generateKey() -> String? {
+        let keyLength = 16
+        let bytes = UnsafeMutablePointer<UInt8>.alloc(keyLength)
+        if SecRandomCopyBytes(kSecRandomDefault, UInt(keyLength), bytes) != 0 {
+            bytes.dealloc(keyLength)
+            return nil
+        }
+        let data = NSData(bytesNoCopy: UnsafeMutablePointer<Void>(bytes), length: keyLength)
+        let ret = data.base64EncodedStringWithOptions(nil)
+        bytes.destroy()
+        return ret
+    }
+
+    private func sendHandshake() {
+        let URL = NSURLComponents(URL: currentRequest.URL, resolvingAgainstBaseURL: true)
+        let host = URL.percentEncodedHost!
+        var resource = URL.percentEncodedPath ?? ""
+        if resource == "" {
+            resource = "/"
+        }
+        if let query = URL.percentEncodedQuery {
+            resource = "\(resource)?\(query)"
+        }
+
+        key = generateKey()
+        if key == nil {
+            NSLog("Could not get random bytes");
+            // FIXME
+            return
+        }
+
+        let handshake = join("\r\n", [
+            "GET \(resource) HTTP/1.1",
+            "Connection: Upgrade",
+            "Host: \(host)",
+            "Sec-WebSocket-Key: \(key!)",
+            "Sec-WebSocket-Version: 13",
+            "Upgrade: websocket",
+            "", ""])
+        NSLog("writing handshake: \n%@", handshake)
+
+        let bytesWritten = output!.write(handshake, maxLength: handshake.lengthOfBytesUsingEncoding(NSASCIIStringEncoding))
+        NSLog("Wrote %@ bytes.", "\(bytesWritten)")
+    }
+}
+
+extension WebSocket {
+    func readData() {
+        let bufferSize = 8192
+        let buffer = UnsafeMutablePointer<UInt8>.alloc(bufferSize)
+        let bytesRead = input!.read(buffer, maxLength: bufferSize)
+        if bytesRead < 0 {
+            NSLog("Error reading from input")
+            return
+        }
+        let data = NSData(bytesNoCopy: UnsafeMutablePointer<Void>(buffer), length: bytesRead > 0 ? bytesRead : bufferSize)
+        let response = NSString(data: data, encoding: NSASCIIStringEncoding)
+        buffer.destroy()
+        NSLog("Got response:\n%@", response)
+    }
+}
+
+extension WebSocket: NSStreamDelegate {
+    public func stream(stream: NSStream, handleEvent streamEvent: NSStreamEvent) {
+        if streamEvent & .ErrorOccurred == .ErrorOccurred {
+            NSLog("Error occurred: %@", stream.streamError!)
+        }
+        if streamEvent & .HasSpaceAvailable == .HasSpaceAvailable && stream == output {
+            if key == nil {
+                sendHandshake()
+            }
+        }
+        if streamEvent & .HasBytesAvailable == .HasBytesAvailable && stream == input {
+            readData()
+        }
     }
 }
 
